18a377f7df73629b9f1f4100e053ea91eaea5193
sajeebro <sajithmadusanka720@gmail.com>
2023-10-24 22:35:13 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f2/d03f41a08d72001e1590ed04ec4856e2
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>register</title>
+</head>
+<body>
+	<form action = "${pageContext.request.contextPath}/UserRegisterServlet" method = "POST">
+		Name: <input type= "text" name = "name">
+		age: <input type= "number" name = "age">
+		
+		<input type="submit" value = "submit">
+	</form>
+</body>
+</html>
---
+++ jsp_test/src/util/DBconnection.java
@@ -0,0 +1,46 @@
+package util;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+	 // Database URL
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/jsp";
+
+
+ // Database credentials
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "12345";
+    
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            // Load the JDBC driver (
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish the database connection
+            connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+
+            // Check if the connection is successful
+            if (connection != null) {
+                System.out.println("Connected to the database.");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Database driver not found.");
+            e.printStackTrace();
+            
+        } catch (SQLException e) {
+            System.err.println("Database connection failed.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+    	getConnection();
+	}
+}
